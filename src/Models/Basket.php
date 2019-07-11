9fcc43663e29d95008353836782129cace80cabf
Luke Roberts <luke@mewt.eu>
2019-07-11 22:10:26 +0100

    Add getTotal method

--- src/Models/Basket.php
+++ src/Models/Basket.php
@@ -2,9 +2,12 @@
 namespace App\Models;
 
 use App\Exceptions\Missing_Product_Exception;
+use App\Exceptions\Unrecognized_Operator_Exception;
 
 class Basket
 {
+    var $_deliveryCost = 0;
+
     public function __construct($arrProducts, $arrDeliveryRules, $arrOffers)
     {
         $this->arrProducts = $arrProducts;
@@ -32,5 +35,69 @@ class Basket
 
         $this->_basket[] = $strProductCode;
         return true;
+    }
+
+    public function getTotal(): int
+    {
+        $intAccumulator = 0;
+
+        foreach ($this->_basket as $strProductCode) {
+            if (!array_key_exists($strProductCode, $this->arrProducts)) {
+                throw new Missing_Product_Exception($strProductCode);
+            }
+
+            $intAccumulator += $this->arrProducts[$strProductCode];
+        }
+
+        foreach ($this->arrOffers as $arrOffer) {
+            $arrProductQuantities = array_count_values($this->_basket);
+            if ($arrOffer['threshold'] <= $arrProductQuantities[$arrOffer['product']]) {
+                $strDiscount = $arrOffer['discount'];
+                if (false !== strpos($arrOffer['discount'], '%')) {
+                    $strDiscount = str_replace('%', '', $strDiscount);
+                    $intMultiplier = 1 - (((int)$strDiscount) / 100);
+                }
+                $intAccumulator -= $intMultiplier * $this->arrProducts[$arrOffer['product']];
+            }
+        }
+
+        $bFoundDeliveryRule = false;
+        foreach ($this->arrDeliveryRules as $arrDeliveryRule) {
+            if ($bFoundDeliveryRule) {
+                break;
+            }
+            $strOperator = $arrDeliveryRule['operator'];
+            switch ($strOperator) {
+                case '<':
+                    if ($intAccumulator < $arrDeliveryRule['threshold']) {
+                        $this->_deliveryCost += $arrDeliveryRule['price'];
+                        $bFoundDeliveryRule = true;
+                    }
+                    break;
+                case '>':
+                    if ($intAccumulator > $arrDeliveryRule['threshold']) {
+                        $this->_deliveryCost += $arrDeliveryRule['price'];
+                        $bFoundDeliveryRule = true;
+                    }
+                    break;
+                case '<=':
+                    if ($intAccumulator <= $arrDeliveryRule['threshold']) {
+                        $this->_deliveryCost += $arrDeliveryRule['price'];
+                        $bFoundDeliveryRule = true;
+                    }
+                    break;
+                case '>=':
+                    if ($intAccumulator <= $arrDeliveryRule['threshold']) {
+                        $this->_deliveryCost += $arrDeliveryRule['price'];
+                        $bFoundDeliveryRule = true;
+                    }
+                    break;
+                default:
+                    throw new Unrecognized_Operator_Exception($arrDeliveryRule['operator']);
+                    break;
+            }
+        }
+
+        return $intAccumulator + $this->_deliveryCost;
     }
 }
